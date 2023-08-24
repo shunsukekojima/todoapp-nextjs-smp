c5d48f2ae9d5f33518342214b1b3ddc1807c38aa
shunsukekojima <127291640+shunsukekojima@users.noreply.github.com>
2023-08-24 20:09:39 +0900

    Api修正

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -1,8 +1,7 @@
-import { env } from "process";
 import { Todo } from "./type";
 
 export const getALLTodos = async ():Promise<Todo> => {
-    const res = await fetch();
+    const res = await fetch('http://localhost:300/api/gettask');
     const todos = await res.json()
 
     return todos;
