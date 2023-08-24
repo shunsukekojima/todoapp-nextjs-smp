2b9873e8a88a3070fc063b09c0cc9bc64509c8ae
shunsukekojima <127291640+shunsukekojima@users.noreply.github.com>
2023-08-24 20:18:01 +0900

    Api fetch

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -1,7 +1,9 @@
 import { Todo } from "./type";
 
 export const getALLTodos = async ():Promise<Todo> => {
-    const res = await fetch('http://localhost:300/api/gettask');
+    const res = await fetch('http://localhost:3000/api/gettask',{
+        cache:"no-store"
+    });
     const todos = await res.json()
 
     return todos;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import AddTask from "./components/AddTask";
 import TodoList from "./components/TodoList";
+import { getALLTodos } from "./api/api";
 
-export default function Home() {
+export default async function Home() {
+    const tasks = await getALLTodos();
+    console.log(tasks);
+
     return (
         <main className={styles.main}>
             <h1>Next.js13 Todo app</h1>
